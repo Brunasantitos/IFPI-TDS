3f592f96275eb05b5182b2de58126c82148b3917
Brunasantitos <santitos@outlook.com.br>
2022-12-19 16:41:53 -0300

    atividade desenvolvida coom o uso de IF, ELIF, e ELSE (condições aninhadas)

---
+++ moduloI/pec/atividade_6sem/Q1.py
@@ -0,0 +1,25 @@
+def valor_vogal(caractere):
+    if caractere == 'a':
+        print('verdadeiro')
+
+    elif caractere == 'e':
+        print('verdaddeiro')
+
+    elif caractere == 'i':
+        print('verdadeiro')
+
+    elif caractere == 'o':
+        print('verdadeiro')
+
+    elif caractere == 'u':
+        print('verdadeiro')
+
+    else:
+        print('falso')
+        
+def main():
+    caractere = input()
+    valor_vogal(caractere)
+
+if __name__=='__main__':
+    main()
