e2e2aa9da2e1f3e3badf2e02b370340207b953d4
Brunasantitos <santitos@outlook.com.br>
2022-12-26 13:31:34 -0300

    projeto em andamento: adicionado somente

--- moduloII/front_end/2022.2/gastos_abastecimento_viagem/js/raciocinio.js
+++ moduloII/front_end/2022.2/gastos_abastecimento_viagem/js/raciocinio.js
@@ -1,21 +1,21 @@
 /* função para se conectar aos butões do html, conhecido mais como evento*/
 
-function clicks(){
+function main(){
     const valor_alcool = document.getElementById ('alcool');
     const valor_gasolina = document.getElementById ('gasolina');
-    const valor_km_por_litro = documente.getElementById ('km_por_litrp');
-    const valor_dinheiro = documente.getElementById ('dinheiro');
-    const valor_distancia = documente.getElementById ('km_totais');
+    const valor_km_por_litro = document.getElementById ('km_por_litro');
+    const valor_dinheiro = document.getElementById ('dinheiro');
+    const valor_distancia = document.getElementById ('km_totais');
 
 
-    const bnt_limpar = documente.getElementById ('limpar');
+    /*const bnt_limpar = documente.getElementById ('limpar');*/
     const bnt_enviar = documente.getElementById ('enviar');
 
 
 
     /* adicionar os valores dos botões após receber seus valores */
 
-/*deve se informar o valor de ONCLIK para reset e deve se realizar a lógica dentro da ação do botão BTN_LIMPAR */
+/*deve se informar o valor de ONCLIK para reset e deve se realizar a lógica dentro da ação do botão BTN_LIMPAR 
 
     bnt_limpar.onreset = () =>{
         const acao_alcool =  (valor_alcool.value);
@@ -24,6 +24,8 @@ function clicks(){
         const acao_valor_dinheiro =  (valor_dinheiro.value);
         const acao_valor_distancia =  (valor_distancia.value);
     }
+
+*/
 /*deve se informar o valor de ONCLIK para ENVIAR e deve se realizar a lógica dentro da ação do botão BTN_ENVIAR */
 
     bnt_enviar.onclick = () => {
@@ -32,6 +34,22 @@ function clicks(){
         const acao_km_por_litro = Number (valor_km_por_litro.value);
         const acao_valor_dinheiro = Number (valor_dinheiro.value);
         const acao_valor_distancia = Number (valor_distancia.value);
+
+        const qntLitrosGasolina = acao_valor_dinheiro / acao_valor_distancia
+
+        const gastoGasolina = qntLitrosGasolina  * acao_gasolina
+
+        const OrcamentoViagem = acao_km_por_litro >= gastoGasolina 
+
+        alert ( `Você precisará de ${ qntLitrosGasolina . toFixed ( 1 ) } L de gasolina.` )
+        alert ( `Você gastará ${ gastoGasolina . toFixed ( 2 ) } Reais com Gasolina` )
+
+
+        if  ( OrcamentoViagem) {
+            alert ( 'Seu orçamento é Suficiente' )
+        } else{
+            alert ( 'Seu orçamento é Insuficiente!' )
+        }
     }
     
 }
