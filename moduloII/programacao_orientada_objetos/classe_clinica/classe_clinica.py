363d1c391d5c7b9f42465a0f350b11bcae5fb94d
Brunasantitos <santitos@outlook.com.br>
2023-03-22 11:15:09 -0300

    CLASSE CLINICA: nova consulta ok, pagar consulta ok, cancelar consulta ok, agendar retorno EM ANDAMENTO

--- moduloII/programacao_orientada_objetos/classe_clinica/classe_clinica.py
+++ moduloII/programacao_orientada_objetos/classe_clinica/classe_clinica.py
@@ -1,8 +1,10 @@
 from datetime import *
+
 class ConsultaMedica:
   dia_semana = {0:'segunda-feira',1:'terça-feira',2:'quarta-feira',3:'quinta-feira',4:'sexta-feira'}
+
   def __init__(self,data_consulta,medico,paciente):
-    fds = [5,6]
+    finalDeSemana = [5,6]
     self.data_consulta = data_consulta
     self.data_retorno = None
     self.medico = medico
@@ -10,43 +12,64 @@ class ConsultaMedica:
     self.pago = False
     self.cancelado = False
 
-    d = datetime.strptime(data_consulta,"%d/%m/%Y").date()
-    if d <= date.today() or d.weekday() in fds:
-                raise ValueError("data de consulta menor que data atual ou caiu em final de semana")
-                print("Valor:", data)
+    dataAtual = datetime.strptime(data_consulta,"%d/%m/%Y").date()
+
+    if dataAtual <= date.today() or dataAtual.weekday() in finalDeSemana:
+        raise ValueError("data de consulta menor que data atual ou caiu em final de semana")
+        print("Valor:", dataAtual)
+
     else:
-      self.data_consulta = datetime.strptime(data_consulta,"%d/%m/%Y").date()
-      self.medico = medico
+        self.data_consulta = datetime.strptime(data_consulta,"%d/%m/%Y").date()
+        self.medico = medico
+
   def __str__(self):
-    return f'data da consulta: {self.data_consulta} {ConsultaMedica.dia_semana[self.data_consulta.weekday()]}'
+        return f'data da consulta: {self.data_consulta} {ConsultaMedica.dia_semana[self.data_consulta.weekday()]}'
 
   def pagar_consulta(self):
-      self.pago = True
+    self.pago = True
+
+  def cancelar_consulta(self):
+    self.cancelado = True
 
 def main():
     consultas = []
+    cancelado = []
     while True:
-        print('1-Nova Consulta')
+        print('\n1-Nova Consulta')
         print('2-Pagar Consulta')
+        print('3-Cancelar Consulta')
 
-        op = input("opção:")
-        if op=='1':
-            consultas.append(ConsultaMedica(input("entre com a data da consulta:(dd/mm/aaaa):"),input("entre com o nome do médico:"),input("entre com o nome do paciente")))
+        MenuAtendimento = input("opção:")
+        if MenuAtendimento=='1':
+            consultas.append(ConsultaMedica(input("entre com a data da consulta:(dd/mm/aaaa): "),input("entre com o nome do médico: "),input("entre com o nome do paciente: ")))
       
-        elif op=='2': 
+        elif MenuAtendimento=='2': 
             cont=0
-        for i,j in enumerate(consultas):
-      #seq+=1
-            if j.pago==False:
+            for i,j in enumerate(consultas):
+              #seq+=1
+              if j.pago == False:
                 cont+=1
                 print(i,j)
-                if cont >0:
-                    op1 = int(input("escolha um indice correspondente a consulta:"))
-                    consultas[op1].pago = True
-                else:
-                    print("Não existem consultas a serem pagas")   
-            elif op=='3':
-                break
+            if cont > 0:
+              op1 = int(input("\nescolha um indice correspondente a consulta: "))
+              consultas[op1].pago = True
+            else:
+              print("\nNão existem consultas a serem pagas")
+              
+        elif MenuAtendimento=='3':
+          cont=0
+          for i,j in enumerate(consultas):
+              #seq+=1
+            if j.cancelado == False:
+              cont+=1
+              print(i,j)
+          if cont > 0:
+            op1 = int(input("\nescolha um indice correspondente a consulta: "))
+            consultas[op1].cancelado = True
+            print("\nConsulta cancelada")
+          else:
+            print("\nNão há consultas para serem canceladas")
+            break
      
     
 if __name__=='__main__':
