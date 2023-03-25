bae89049134fb2a0618e2bc4f61ce2f1f1ffd0ac
Brunasantitos <santitos@outlook.com.br>
2023-03-25 14:40:42 -0300

    atividade sendo finalizada

--- moduloII/desenvolvimento_orientado_testes/lista_1/lista1_q10.py
+++ moduloII/desenvolvimento_orientado_testes/lista_1/lista1_q10.py
@@ -1,12 +1,21 @@
-def maximo(n1, n2):
-    o_maior = (max(n1, n2))
-    print(f'\nO maior número é {o_maior}')
+def MaiorNumero(A, B, C, D):
+    maior = max(A, B, C, D)
+    print(f'O maior número é {maior}')
 
 def main():
-    numero1 = int(input("Informe um valor: "))
-    numero2 = int(input("Informe um número: "))
+    contador = 0
+    cont = 1
+    while contador < 4:
+        print(f'\nO MAIOR NÚMERO DA SÉRIE {cont}:')
+        cont += 1
+        A = int(input("Informe um número: "))
+        B = int(input("Informe um número: "))
+        C = int(input("Informe um número: "))
+        D = int(input("Informe um número: "))
 
-    maximo(numero1, numero2)
+        contador += 1
+        MaiorNumero(A,B,C,D)
+            
 
 if __name__=='__main__':
     main()
