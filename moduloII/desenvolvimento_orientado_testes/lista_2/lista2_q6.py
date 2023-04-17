4a2b323ca993350892ce043b322648c7b0c58fe5
Brunasantitos <santitos@outlook.com.br>
2023-04-17 14:53:20 -0300

    atividade em andamento

--- moduloII/desenvolvimento_orientado_testes/lista_2/lista2_q6.py
+++ moduloII/desenvolvimento_orientado_testes/lista_2/lista2_q6.py
@@ -1,9 +1,16 @@
-def faturamento(L1):
+def faturamento(L1,L2):
     pass
 
 def main():
+    ListaQuantidade = []
     ListaValores = []
-
+    contador = 0
+    while contador<=20:
+        ListaQuantidade.append(int(input('Informe a quantidade de produto: ')))
+        ListaValores.append(float(input('Informe o valor do produto: ')))
+        faturamento(ListaQuantidade,ListaValores)
+        contador+=1
+        
 
 if __name__=='__main__':
     main()
