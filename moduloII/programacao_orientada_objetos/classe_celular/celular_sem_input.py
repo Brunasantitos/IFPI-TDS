b59d5542042448942daafb8516a1c98c5f6ff5fe
Brunasantitos <santitos@outlook.com.br>
2023-06-17 21:48:13 -0300

    falta associar as classes|

--- moduloII/programacao_orientada_objetos/classe_celular/celular_sem_input.py
+++ moduloII/programacao_orientada_objetos/classe_celular/celular_sem_input.py
@@ -5,7 +5,7 @@ class Bateria:
         self.__codigo = "A37"
         self.capacidade = capacidade
         self.__percentual_carga = random.randint(0,100)
-
+    
     @property
     def codigo(self):
         return self.__codigo
@@ -13,17 +13,17 @@ class Bateria:
     @property
     def percentual_carga(self):
         return self.__percentual_carga
-
+    
 
     def carregar(self,valor):
 
         if valor >= 0:
+            self.__percentual_carga = 100
             if valor < 100:
-                print(f'bateria em {valor}%')
-                self.__percentual_carga = 100
-                print("carregando...")
+                print ("carregando...")
+                
             else:
-                print("bateria em 100%")
+                return self.__percentual_carga
         else:
             raise "error" 
             
@@ -33,26 +33,28 @@ class Bateria:
         if valor >= 0:
             if valor == 0:
                 self.__percentual_carga = valor
-                print("descarregado")
-                print(f'{self.__percentual_carga}%')
+                print(f'celular descarregado')
+            
+                
 
             elif valor < 100:
                 valor -= 1
                 self.__percentual_carga = valor
-                print(f'bateria em {self.__percentual_carga}%')
-                print("descarregando...")
+                return self.__percentual_carga
+                
         else:
             raise "error"
         
-        
+    def __str__(self):
+        return f'bateria em {self.__percentual_carga}%'
 
 class Celular:
-    def __init__(self, bateria1):
+    def __init__(self, bateria):
         self.__mei = 14536789
-        self.bateria = bateria1
+        self.bateria = bateria
         self.__wifi = False
         self.__ligar = False        
-
+    
     @property
     def mei(self):
         return self.__mei
@@ -71,19 +73,26 @@ class Celular:
         if self.bateria > 0:
 
             self.__ligar = True
-            print(f'{self.bateria}%')
-            print("Ligando...")
+            print (f'Celular ligado')
 
-        elif self.__bateria == 0:
-            print("SEM BATERIA")
+        elif self.bateria == 0:
+            print ("SEM BATERIA")
 
 
+        elif self.__ligar == True:
+            self.__ligar = False
+            print('Celular desligado')
+
+    def __str__(self) -> str:
+        return f'{self.__ligar}'
     
 
 def main():
-    bateria1 = Bateria(50)
-    celular1 = Celular(bateria1)
-
+    b1 = Bateria(50)
+    celular1 =  Celular(b1)
+    celular1.ligarDesligar()
+    b1.carregar(100)
+    print(b1)
     print(celular1)
     
 
