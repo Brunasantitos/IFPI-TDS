9e58cfbd4b813ca57217d9e54351c48eeb5476d8
Brunasantitos <santitos@outlook.com.br>
2023-06-24 15:54:41 -0300

    atualizando classe universidade

--- moduloII/programacao_orientada_objetos/classe_universidade/universidade.py
+++ moduloII/programacao_orientada_objetos/classe_universidade/universidade.py
@@ -5,7 +5,6 @@ class Sisu(object):
         if type(universidade)== universidade:
             Sisu.__universidades.append(universidade)
       
-  
     def busca_universidade(nome):
         for i in Sisu.__universidades:
             if i.nome == nome:
@@ -19,15 +18,19 @@ class Universidade:
         self.__nome = nome
         self.__tipo = tipo
         self.__cursos = []
+
     @property
     def sigla(self):
         return self.__sigla
+    
     @property
     def nome(self):
         return self.__nome
+    
     @property
     def tipo(self):
         return self.__tipo
+    
     @property
     def cursos(self):
         return self.__cursos
@@ -39,14 +42,17 @@ class Universidade:
         else:
             print("Erro!")
       
-  
     def buscar_curso(self,curso):
         for i in self.__alunos:
             if i.curso == curso:
                 return i
         return None
+    
+    def matricula_aluno(self,aluno,curso):
+        if self.buscar_curso(curso.nome)!=None:
+            if aluno.nota_enem >= curso.nota_corte:
+                pass
          
-
     def __str__(self):
         cab = f'{self.__sigla}- Relação de alunos\n'
         dados=''
@@ -55,22 +61,30 @@ class Universidade:
         return cab+dados
 
 class Curso:
-    def __init__(self,id,nome,duracao,vagas,nota_corte):
+    def __init__(self,id,nome,vagas,nota_corte):
         self.__id = id
-        self.nome = nome
-        self.duracao = duracao
-        self.vagas = vagas
-        self.nota_corte = nota_corte
+        self.__nome = nome
+        self.__vagas = vagas
+        self.__nota_corte = nota_corte
+        self.alunos = []
 
     @property
     def id(self):
         return self.__id
     
-    def cadastrar_aluno(self):
+    def incluir_aluno(self):
         pass
 
+    def buscar_aluno(self):
+        pass
 
+    def solicitar_entrada(self):
+        pass
 
+    def __str__(self):
+        cab = f'curso:{self.__nome} - Relação de alunos'
+        for i in self.__alunos:
+            pass
 
 class Aluno:
     def __init__(self,cpf,nome,dt_nasc,ponto_enem):
@@ -80,20 +94,25 @@ class Aluno:
         self.ponto_enem = ponto_enem
         self.matricula_publica = False
     
-    
     @property
     def cpf(self):
         return self.__cpf
-    
 
     def solicita_entrada(self,curso,universidade):
         if type(curso) == Curso and type(universidade) == Universidade:
             if self.ponto_enem:
                 pass
 
+    def efetivar_matricula(self,curso,universidade):
+        #universidade.matricula_aluno(self,curso)
+        if self.solicita_entrada(curso,universidade):
+            pass
+
+    def solita_transferencia(self,univ_ori,curso_ori,univ_dest):
+        pass
     
     def __str__(self):
-        pass    
+        pass  
 
 def main():
     maria = Aluno ("11111111111111","Maria","01/02/1990")
